81050a893a9195293ba1f0c2226a566625d64852
Caio Bacelar <caiobacelarM@gmail.com>
2026-03-18 22:17:53 -0300

    feat: Criação do DbContext da aplicação
    -Implementado AppDbContext com DbSet de Produtos para integração com o Entity Framework;
    chore: Adição do Entity Framework Core ao projeto
    Adicionei os pacotes do Entity Framework Core e SQL Server para persistência de dados., pois precisava baixar no meu Visual Studio.

---
+++ Backend/CatalogoProdutos.Api/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CatalogoProdutos.API.Domain.Entities;
+
+namespace CatalogoProdutos.API.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
