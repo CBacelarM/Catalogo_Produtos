669d4a11750a295335646821458f4b680d67879e
Caio Bacelar <caiobacelarM@gmail.com>
2026-03-18 23:20:48 -0300

    feat: Configuração de serviços e middlewares da aplicação
    Configurado DbContext, Swagger e política de CORS para permitir integração com o frontend.
    
    feat: Configuração da string de conexão com banco de dados
    Adicionada connection string utilizando SQL Server LocalDB.

--- Backend/CatalogoProdutos.Api/Program.cs
+++ Backend/CatalogoProdutos.Api/Program.cs
@@ -1,20 +1,30 @@
+using CatalogoProdutos.API.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+    );
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
