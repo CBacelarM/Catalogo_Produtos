cf28aea040dd1020d876d5cfa5b81f76d138abe9
Caio Bacelar <caiobacelarM@gmail.com>
2026-03-23 14:37:51 -0300

    feat: Implementação de teste unitários básicos na API de produtos

---
+++ Backend/CatalogoProdutos.Tests/ProdutosControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace CatalogoProdutos.Tests;
+
+public class ProdutosControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public ProdutosControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetProdutos_DeveRetornarOk()
+    {
+        var response = await _client.GetAsync("/produtos");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
